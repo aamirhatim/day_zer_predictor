7dfbbcd11a9de0ad2c4942bee4669d8df6271ab5
Ahalya Mandana <31750142+am2512@users.noreply.github.com>
2018-05-21 15:08:35 -0500

    Python script to filter the Water Resources CSV

---
+++ water_resources.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+
+df = pd.read_csv('water_resources.csv')
+df2 = df.loc[(df['Variable Name'] == 'Total renewable water resources per capita') & (df['Year'] == 2015)]
+print(df2)
