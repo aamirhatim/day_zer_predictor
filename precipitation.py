8a309f4101662ce2ae3dea2b29c7f870f2e075ab
Aamir Husain <aamir300@gmail.com>
2018-05-20 23:48:37 -0500

    Working on removing rows from data

--- precipitation.py
+++ precipitation.py
@@ -7,7 +7,16 @@ import pandas as pd
 import numpy as np
 
 def main():
-    print("hi")
+    filename = 'precipitation_AQUASTAT.csv'
+    path = 'data/'+filename
+
+    precip = pd.read_csv(path)
+    delvalues = ['Md', 'Symbol', 'Area Id', 'Variable Id']
+    for x in delvalues:
+        del precip[x]
+
+    # ind = precip[precip.loc['Metadata:']].index.values.astype(int)
+    print(precip.info())
 
 if __name__ == "__main__":
     main()
