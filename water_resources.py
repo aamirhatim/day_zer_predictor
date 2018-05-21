cdc4d48c1be3be667c145902e38cbe81d92b7459
Ahalya Mandana <31750142+am2512@users.noreply.github.com>
2018-05-21 15:09:15 -0500

    Update water_resources.py

--- water_resources.py
+++ water_resources.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 
 df = pd.read_csv('water_resources.csv')
-df2 = df.loc[(df['Variable Name'] == 'Total renewable water resources per capita') & (df['Year'] == 2015)]
+df2 = df.loc[(df['Variable Name'] == 'Total renewable water resources per capita') & (df['Year'] == 2014)]
 print(df2)
