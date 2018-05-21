ad3f3a6160651a7d967440d25275e4c8ebd5cde2
Aamir Husain <aamir300@gmail.com>
2018-05-20 19:20:31 -0500

    Set up pandas query for precipitation data

---
+++ precipitation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.6
+'''
+PANDAS code to query the precipitation csv from AQUASTAT
+'''
+
+import pandas as pd
+import numpy as np
+
+def main():
+    print("hi")
+
+if __name__ == "__main__":
+    main()
