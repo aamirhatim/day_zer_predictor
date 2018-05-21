1f43d5abc89079072f5092b0d66ec63a7c9060f1
Ahalya Mandana <31750142+am2512@users.noreply.github.com>
2018-05-21 15:38:19 -0500

    Update water_resources.py

--- water_resources.py
+++ water_resources.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 
 
-df = pd.read_csv('water_resources.csv')
+df = pd.read_csv('water_resources_revised.csv')
 df2 = df.loc[(df['Variable Name'] == 'Total renewable water resources per capita') & (df['Year'] == 2014)]
+df2 = df2.drop(['Variable Name'], axis = 1)
+df2.to_csv('new_water_resources.csv', sep='\t')
 print(df2)
