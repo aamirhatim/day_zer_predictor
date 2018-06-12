22879466563cb841c4fb42a77c0e9126c98637a7
Ahalya Mandana <ahalyamandana@gmail.com>
2018-06-12 16:09:14 -0500

    Adding script to generate a table of countries and corresponding Year Zero

---
+++ country_data.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import sys
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+
+
+def export(table, out_path):
+    table.to_csv(out_path, index = False)
+    print('CSV exported to:', out_path)
+
+
+#def collect_data():
+#     # Import CSVs
+country_stress = pd.read_csv('data/lookup_table.csv', index_col = False)
+country_list = (country_stress.Country.unique())
+critical_yearlist = []
+critical_year = 0
+for i in country_list:
+    #print(i)
+    data = country_stress[country_stress['Country']==i]
+    #print(data)
+    final_data = data.ix[:, ['Year', 'stress']]
+    final_data.loc[-1] = [1860,0]
+    final_data.index = final_data.index+1
+    final_data = final_data.sort_index()
+    #print(final_data)
+
+    export(final_data, 'data/stress.csv')
+
+    data = pd.read_csv('data/stress.csv')
+    data.head()
+
+    X = data['Year'].values[:,np.newaxis]
+    y = data['stress'].values
+
+    model = LinearRegression()
+    model.fit(X, y)
+
+    slope = model.coef_
+    x_intercept = model.intercept_
+    #print(model.intercept_)
+    #print(model.coef_)
+
+    critical_year = (7-x_intercept)/slope
+    print(critical_year)
+    critical_yearlist.append(critical_year[0])
+
+clean_stress = pd.DataFrame({'Country':country_list,'YearZero':critical_yearlist})
+export(clean_stress,'data/final_stress.csv')
--- polynomial regression/poly_reg.py
+++ polynomial regression/poly_reg.py
@@ -16,24 +16,36 @@ y = data['Stress'].values
 model = LinearRegression()
 model.fit(X, y)
 
-#polynomial regression, degree 6
-model1 = Pipeline([('poly', PolynomialFeatures(degree=6)),
+# polynomial regression, degree 6
+model1 = Pipeline([('poly', PolynomialFeatures(degree=3)),
                   ('linear', LinearRegression(fit_intercept=False))])
 model1.fit(X,y)
-print(model1.named_steps['linear'].coef_)
+#print(model1.named_steps['linear'].coef_)
+c = model1.named_steps['linear'].coef_
+#print(c)
+
+# year = np.poly1d(c)
+
+# print(year(6))
+
+ 
+
 
 
 #finding X intercept for linear regression
-# slope = model.coef_
-# x_intercept = model.intercept_
-# print(model.intercept_)
-# print(model.coef_)
-#
-# critical_year = (100-x_intercept)/slope
-# print("Year of critical water shortage:", critical_year)
-#
-#
+slope = model.coef_
+x_intercept = model.intercept_
+print(model.intercept_)
+print(model.coef_)
+
+critical_year = (7-x_intercept)/slope
+print("Year of critical water shortage:", critical_year)
+
+
+
+
 #
 plt.scatter(X, y,color='r')
-plt.plot(X, model1.predict(X),color='k')
+plt.plot(X, model.predict(X),color='k')
+
 plt.show()
