8104c065874a8163a7c26df2d552591faeee37ab
Ahalya Mandana <ahalyamandana@gmail.com>
2018-06-12 23:44:35 -0500

    Updating Project Description on main webpage

--- Project Data/polynomial regression/poly_reg.py
+++ Project Data/polynomial regression/poly_reg.py
@@ -7,7 +7,6 @@ from sklearn.pipeline import Pipeline
 
 from datetime import timedelta, datetime
 
-
 def convert_partial_year(number):
 
     year = int(number)
@@ -15,6 +14,7 @@ def convert_partial_year(number):
     day_one = datetime(year,1,1)
     date = d + day_one
     return date
+
 
 
 
@@ -51,21 +51,8 @@ print(model.coef_)
 
 critical_year = (7-x_intercept)/slope
 print("Year of critical water shortage:", critical_year)
-ddmmyy = convert_partial_year(critical_year[0])
-#print(ddmmyy)
-
-format = "%a %b %d %Y"
-
-today = datetime.today()
-#print('ISO     :', today)
-
-s = today.strftime(format)
-#print('strftime:', s)
-
-d = datetime.strptime(s, format)
-day_zero = ddmmyy.strftime(format)
-print(day_zero)
-
+ddmmyy = convert_partial_year(critical_year)
+print(ddmmyy)
 
 
 
